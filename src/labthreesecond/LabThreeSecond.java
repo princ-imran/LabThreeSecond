cd6b5eb81d0150ec8eacb29d7bb8db2af4814b84
Imran Hoshain <princ.imran@gmail.com>
2020-03-02 10:08:57 +0600

    Java Lab Three Seond

---
+++ src/labthreesecond/LabThreeSecond.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labthreesecond;
+
+/**
+ *
+ * @author Spy Hacker
+ */
+public class LabThreeSecond {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num[] = {31, 28, 31, 30};        
+        int cal = 0, i;
+        for(i=0; 0<5; i++){
+            cal = num[i]+cal;
+        }
+        System.out.println(cal[0]);
+    }
+    
+}
